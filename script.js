29e2f3dfc2669a11fcae3a02e9f6e840d46b14b1
Ft-sego <nkabilesego@gmail.com>
2026-02-05 19:59:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const envelope = document.getElementById("envelope");
 
-  if (!envelope) return;
+  if (!envelope) {
+    console.error("Envelope image not found");
+    return;
+  }
 
   envelope.addEventListener("click", () => {
     window.location.href = "original_site/index.html";
